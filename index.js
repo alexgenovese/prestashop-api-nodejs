f8ff1cfd49b5577cd7e8235ba681b234611a24e4
Alex Genovese <423745+alexgenovese@users.noreply.github.com>
2021-04-28 18:35:14 +0200

    .

---
+++ index.js
@@ -0,0 +1,124 @@
+const request = require('request');
+const httpBuildQuery = require('http-build-query');
+var nodeBase64 = require('nodejs-base64-converter');
+
+const params = ['filter', 'display', 'sort', 'limit', 'output_format', 'schema', 'id_shop', 'id_group_shop', 'ws_key'];
+
+const req = (opt) => {
+    return new Promise((resolve) => {
+        request(opt, (err, res, body) => {
+            resolve({
+                err: err,
+                res: res,
+                body: body
+            });
+        });
+    });
+};
+
+const exec = async (opt) => {
+    let {err, res, body} = await req(opt);
+    if(err) return {status_code: 500, response: null, headers: null}
+    return {
+        status_code: res.statusCode,
+        response: body,
+        headers: res.headers
+    };
+};
+
+const buildQuery = (opt) => {
+    let url_params = {};
+    let query = '';
+    for(let param of params) {
+        for(let [key, value] of Object.entries(opt)) {
+            if(key === param || key.includes(param)) {
+                url_params[key] = opt[key];
+            }
+        }
+    }
+    if(Object.entries(url_params).length > 0) {
+        query = `?${httpBuildQuery(url_params)}`;
+    }
+    return query;
+};
+
+const buildRoute = (options, opt) => {
+    let route = `http://${options.url}/api/${opt['resource']}`;
+    if(opt['id']) route = `${route}/${opt['id']}`;
+    return route;
+};
+
+const buildUrl = (options, opt) => {
+    let route = buildRoute(options, opt);
+    let query = buildQuery(opt);
+    url = `${route}${query}`;
+    return url;
+};
+
+module.exports = function( options ) {
+    const base64_key = nodeBase64.encode(options.key + ':')
+
+    this.post = async (opt) => {
+        let url = buildUrl(options, opt);
+        let body = opt['body'];
+        let req = await exec({
+            url: url,
+            method: 'POST',
+            headers: {
+                'Authorization': 'Basic ' + base64_key,
+                'Content-Type': 'application/json'
+            },
+            body: opt['output_format'] === 'JSON' ? JSON.stringify(body) : body
+        });
+        return opt['output_format'] === 'JSON' ? JSON.parse(req['response']) : req['response'];
+    };
+    this.get = async (opt) => {
+        let url = buildUrl(options, opt);
+        let req = await exec({
+            url: url,
+            method: 'GET',
+            headers: {
+                'Authorization': 'Basic ' + base64_key,
+                'Content-Type': 'application/json'
+            }
+        });
+        return opt['output_format'] === 'JSON' ? JSON.parse(req['response']) : req['response'];
+    };
+    this.head = async (opt) => {
+        let url = buildUrl(options, opt);
+        let req = await exec({
+            url: url,
+            method: 'HEAD',
+            headers: {
+                'Authorization': 'Basic ' + base64_key,
+                'Content-Type': 'application/json'
+            }
+        });
+        return req.response
+    };
+    this.put = async (opt) => {
+        let url = buildUrl(options, opt);
+        let xml = opt['body'];
+        let req = await exec({
+            url: url,
+            method: 'PUT',
+            headers: {
+                'Authorization': 'Basic ' + base64_key,
+                'Content-Type': 'application/json'
+            },
+            body: opt['output_format'] === 'JSON' ? JSON.stringify(body) : body
+        });
+        return opt['output_format'] === 'JSON' ? JSON.parse(req['response']) : req['response'];
+    };
+    this.delete = async (opt) => {
+        let url = buildUrl(options, opt);
+        let req = await exec({
+            url: url,
+            method: 'DELETE',
+            headers: {
+                Expect: '100-continue'
+            }
+        });
+        return req.response
+    };
+}
